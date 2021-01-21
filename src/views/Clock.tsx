e5c9420aac04d72e97459515dc905b188d1cd5bf
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-01-22 00:34:35 +0100

    Refactoring: Use shared components etc.

--- src/views/Clock.tsx
+++ src/views/Clock.tsx
@@ -1,31 +1,52 @@
 import { AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
-import { useCountdown } from "../calc";
-import BudgetRemaining from "../components/BudgetRemaining";
-import { Main } from "../components/Layout";
-import { Tab } from "../components/tab";
-import TimeRemaining from "../components/TimeRemaining";
+import styled from "styled-components";
+import { Main, BudgetRemaining, TimeRemaining, TabButton } from "../components";
+import { useCountdown } from "../hooks";
+
+// TODO: Add 2.0 scenario
+
+const TabsContainer = styled.div`
+  position: absolute;
+  bottom: 25vh;
+  z-index: 50;
+`;
 
 export default function Clock() {
-  const { remainingTime, remainingBudget } = useCountdown();
+  const { remainingTime, remainingBudget } = useCountdown(500);
   const [active, setActive] = useState("carbon");
   return (
-    <Main as="main">
-      <AnimatePresence exitBeforeEnter>
+    <Main
+      role="main"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      <AnimatePresence exitBeforeEnter initial={false}>
         {active === "time" ? (
           <TimeRemaining value={remainingTime} key="time" />
         ) : (
           <BudgetRemaining value={remainingBudget} key="budget" />
         )}
       </AnimatePresence>
-      <div style={{ position: "fixed", bottom: 150, zIndex: 100 }}>
-        <Tab isActive={active === "carbon"} onClick={() => setActive("carbon")}>
+      <TabsContainer>
+        <TabButton
+          role="button"
+          tabIndex={0}
+          isActive={active === "carbon"}
+          onClick={() => setActive("carbon")}
+        >
           CO<sub>2</sub> budget left
-        </Tab>
-        <Tab isActive={active === "time"} onClick={() => setActive("time")}>
-          Remaining time
-        </Tab>
-      </div>
+        </TabButton>
+        <TabButton
+          role="button"
+          tabIndex={0}
+          isActive={active === "time"}
+          onClick={() => setActive("time")}
+        >
+          Time remaining
+        </TabButton>
+      </TabsContainer>
     </Main>
   );
 }
