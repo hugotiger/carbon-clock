82e42e46309091f9ea9afde82a7f212b69f5ae7c
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-01-22 00:36:52 +0100

    Added exports file

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as BudgetRemaining } from "./BudgetRemaining";
+export { default as TimeRemaining } from "./TimeRemaining";
+export { default as TabButton } from "./TabButton";
+export { Main, Container } from "./Layout";
+export { default as Header } from "./Header";
