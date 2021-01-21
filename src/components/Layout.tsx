388f4427d9ffdc891d1b514293a3a14c342b646a
Hugo Holm <hugo.tiger.holm@gmail.com>
2021-01-22 00:32:54 +0100

    Added container

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const Main = styled.main`
+export const Main = styled(motion.main)`
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -14,25 +15,24 @@ export const Main = styled.main`
   background-size: cover;
   background-position: center center;
 
-  animation: fadein 2s;
-
-  @keyframes fadein {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
+  position: relative;
 
   &::after {
     content: "";
     position: absolute;
     width: 100%;
     min-height: 100%;
-    background: linear-gradient(transparent 80%, black),
-      radial-gradient(rgba(0, 0, 0, 0) 25%, black);
+    background: linear-gradient(transparent 80%, var(--black)),
+      radial-gradient(rgba(0, 0, 0, 0) 25%, var(--black));
     pointer-events: none;
-    z-index: 1;
+    z-index: 0;
   }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 640px;
+  margin: 0 auto;
+  padding: 0 1rem;
+  margin-bottom: 2rem; //TODO: Remove
 `;
